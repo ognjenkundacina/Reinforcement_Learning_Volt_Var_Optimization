b1bf70be0bb7d23407a469619d4cabc35d7c92c5
lazartimotic <laza5292@gmail.com>
2019-12-01 23:18:02 +0100

    Added method for calculating power losses of the grid

--- power_algorithms/power_flow.py
+++ power_algorithms/power_flow.py
@@ -8,9 +8,18 @@ class PowerFlow:
     def calculate_power_flow(self):
         pp.runpp(self.power_grid, algorithm="bfsw", calculate_voltage_angles=False)
 
+    def get_losses(self):
+        grid_losses = 0
+        for line_losses in self.power_grid.res_line.pl_mw:
+            grid_losses += line_losses
+        for transformer_losses in self.power_grid.res_trafo.pl_mw:
+            grid_losses += transformer_losses
+        
+        return grid_losses
 
 # def main():
 #     power_flow = PowerFlow()
 #     power_flow.calculate_power_flow()
+#     print(power_flow.get_losses())
 
 # main()
