cf3b9bb62a6217f30a493d2663cd21e4b482724b
lazartimotic <laza5292@gmail.com>
2019-12-07 13:07:09 +0100

    Return capacitors status and toogle status methods

--- power_algorithms/network_management.py
+++ power_algorithms/network_management.py
@@ -1,5 +1,6 @@
 import power_algorithms.network_definition as grid
 import pandapower as pp
+import pandas as pd
 
 class NetworkManagement:
     def __init__(self):
@@ -13,6 +14,14 @@ class NetworkManagement:
         switchIndex = pp.get_element_index(self.power_grid, "switch", capSwitchName)
         self.power_grid.switch.closed.loc[switchIndex] = closed
     
+    def toogle_capacitor_status(self, capSwitchName):
+        switchIndex = pp.get_element_index(self.power_grid, "switch", capSwitchName)
+        currentState = self.power_grid.switch.closed.loc[switchIndex]
+        self.power_grid.switch.closed.loc[switchIndex] = not currentState
+
     def get_all_capacitor_switch_names(self):
         return self.power_grid.switch['name'].tolist()
+
+    def get_all_capacitors(self):
+        return pd.Series(self.power_grid.switch.closed.values, index=self.power_grid.switch.name).to_dict()
 
