81c5a61512443d36ba34ed66348336823b3bf54c
lazartimotic <laza5292@gmail.com>
2019-12-07 22:54:44 +0100

    Network parameters changed - two more capacitors added

--- power_algorithms/network_definition.py
+++ power_algorithms/network_definition.py
@@ -19,7 +19,7 @@ def create_network():
     for i in range(0, 100):
         busNodes.append(pp.create_bus(network, vn_kv=20, name="Bus_" + str(i+1)))
         lowVoltageBusNodes.append(pp.create_bus(network, vn_kv=0.4, name="LowVoltageBus_" + str(i+1)))
-        pp.create_load(network, bus=lowVoltageBusNodes[i], p_mw=0.1, q_mvar=0.033, name="Load_" + str(i+1))
+        pp.create_load(network, bus=lowVoltageBusNodes[i], p_mw=0.07, q_mvar=0.025, name="Load_" + str(i+1))
         pp.create_transformer(network, hv_bus=busNodes[i], lv_bus=lowVoltageBusNodes[i], std_type="SN/NN 1MVA", name="Transformer_" + str(i+1))
 
     pp.create_line(network, from_bus=mediumVoltageBusNode, to_bus=busNodes[0], length_km=0.2, name="Line_0", std_type="NA2XS2Y 1x150 RM/25 12/20 kV")
@@ -29,27 +29,35 @@ def create_network():
 
     # Add capacitors with regulating switches
     pp.create_bus(network, vn_kv=20, name="Bus_Cap1")
-    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_18'), pp.get_element_index(network, "bus", 'Bus_Cap1'), et="b", closed=False, type="LBS", name="CapSwitch1")
-    pp.create_shunt_as_capacitor(network, pp.get_element_index(network, "bus", 'Bus_Cap1'), 0.25, 0, name="Cap1")
+    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_13'), pp.get_element_index(network, "bus", 'Bus_Cap1'), et="b", closed=False, type="LBS", name="CapSwitch1")
+    pp.create_shunt_as_capacitor(network, pp.get_element_index(network, "bus", 'Bus_Cap1'), 0.125, 0, name="Cap1")
 
     pp.create_bus(network, vn_kv=20, name="Bus_Cap2")
-    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_33'), pp.get_element_index(network, "bus", 'Bus_Cap2'), et="b", closed=False, type="LBS", name="CapSwitch2")
-    pp.create_shunt_as_capacitor(network, pp.get_element_index(network, "bus", 'Bus_Cap2'), 0.25, 0, name="Cap2")
+    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_39'), pp.get_element_index(network, "bus", 'Bus_Cap2'), et="b", closed=False, type="LBS", name="CapSwitch2")
+    pp.create_shunt_as_capacitor(network, pp.get_element_index(network, "bus", 'Bus_Cap2'), 0.125, 0, name="Cap2")
 
     pp.create_bus(network, vn_kv=20, name="Bus_Cap3")
-    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_48'), pp.get_element_index(network, "bus", 'Bus_Cap3'), et="b", closed=False, type="LBS", name="CapSwitch3")
-    pp.create_shunt_as_capacitor(network, pp.get_element_index(network, "bus", 'Bus_Cap3'), 0.25, 0, name="Cap3")
+    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_85'), pp.get_element_index(network, "bus", 'Bus_Cap3'), et="b", closed=False, type="LBS", name="CapSwitch3")
+    pp.create_shunt_as_capacitor(network, pp.get_element_index(network, "bus", 'Bus_Cap3'), 0.125, 0, name="Cap3")
 
     pp.create_bus(network, vn_kv=20, name="Bus_Cap4")
-    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_63'), pp.get_element_index(network, "bus", 'Bus_Cap4'), et="b", closed=False, type="LBS", name="CapSwitch4")
+    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_28'), pp.get_element_index(network, "bus", 'Bus_Cap4'), et="b", closed=False, type="LBS", name="CapSwitch4")
     pp.create_shunt_as_capacitor(network, pp.get_element_index(network, "bus", 'Bus_Cap4'), 0.25, 0, name="Cap4")
 
     pp.create_bus(network, vn_kv=20, name="Bus_Cap5")
-    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_78'), pp.get_element_index(network, "bus", 'Bus_Cap5'), et="b", closed=False, type="LBS", name="CapSwitch5")
+    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_59'), pp.get_element_index(network, "bus", 'Bus_Cap5'), et="b", closed=False, type="LBS", name="CapSwitch5")
     pp.create_shunt_as_capacitor(network, pp.get_element_index(network, "bus", 'Bus_Cap5'), 0.25, 0, name="Cap5")
 
     pp.create_bus(network, vn_kv=20, name="Bus_Cap6")
-    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_93'), pp.get_element_index(network, "bus", 'Bus_Cap6'), et="b", closed=False, type="LBS", name="CapSwitch6")
+    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_70'), pp.get_element_index(network, "bus", 'Bus_Cap6'), et="b", closed=False, type="LBS", name="CapSwitch6")
     pp.create_shunt_as_capacitor(network, pp.get_element_index(network, "bus", 'Bus_Cap6'), 0.25, 0, name="Cap6")
+
+    pp.create_bus(network, vn_kv=20, name="Bus_Cap7")
+    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_48'), pp.get_element_index(network, "bus", 'Bus_Cap7'), et="b", closed=False, type="LBS", name="CapSwitch7")
+    pp.create_shunt_as_capacitor(network, pp.get_element_index(network, "bus", 'Bus_Cap7'), 0.6, 0, name="Cap7")
+
+    pp.create_bus(network, vn_kv=20, name="Bus_Cap8")
+    pp.create_switch(network, pp.get_element_index(network, "bus", 'Bus_95'), pp.get_element_index(network, "bus", 'Bus_Cap8'), et="b", closed=False, type="LBS", name="CapSwitch8")
+    pp.create_shunt_as_capacitor(network, pp.get_element_index(network, "bus", 'Bus_Cap8'), 0.6, 0, name="Cap8")
 
     return network
--- power_algorithms/vvo_brute_force.py
+++ power_algorithms/vvo_brute_force.py
@@ -17,7 +17,7 @@ def vvo_brute_force(df_test):
         power_flow.calculate_power_flow()
         min_losses = power_flow.get_losses()
         print ('Starting losses: ', min_losses)
-        optimal_statuses = [False, False, False, False, False, False]
+        optimal_statuses = [False, False, False, False, False, False, False, False]
 
         for a in False, True:
             for b in False, True:
@@ -25,20 +25,24 @@ def vvo_brute_force(df_test):
                     for d in False, True:
                         for e in False, True:
                             for f in False, True:
-                                network_manager.change_capacitor_status('CapSwitch1', a)
-                                network_manager.change_capacitor_status('CapSwitch2', b)
-                                network_manager.change_capacitor_status('CapSwitch3', c)
-                                network_manager.change_capacitor_status('CapSwitch4', d)
-                                network_manager.change_capacitor_status('CapSwitch5', e)
-                                network_manager.change_capacitor_status('CapSwitch6', f)
-                                power_flow.calculate_power_flow()
-                                current_losses = power_flow.get_losses()
-                                if (current_losses < min_losses):
-                                    min_losses = current_losses
-                                    optimal_statuses = [a, b, c, d, e, f]
+                                for g in False, True:
+                                    for h in False, True:
+                                        network_manager.change_capacitor_status('CapSwitch1', a)
+                                        network_manager.change_capacitor_status('CapSwitch2', b)
+                                        network_manager.change_capacitor_status('CapSwitch3', c)
+                                        network_manager.change_capacitor_status('CapSwitch4', d)
+                                        network_manager.change_capacitor_status('CapSwitch5', e)
+                                        network_manager.change_capacitor_status('CapSwitch6', f)
+                                        network_manager.change_capacitor_status('CapSwitch7', g)
+                                        network_manager.change_capacitor_status('CapSwitch8', h)
+                                        power_flow.calculate_power_flow()
+                                        current_losses = power_flow.get_losses()
+                                        if (current_losses < min_losses):
+                                            min_losses = current_losses
+                                            optimal_statuses = [a, b, c, d, e, f, g, h]
 
         print ('Minimal losses: ', min_losses)
         print('Minimal losses are achieved when the following capacitors are ON:')
-        for i in range(6):
+        for i in range(8):
             if (optimal_statuses[i] != capacitor_statuses[i]):
                 print("CapSwitch", i+1, " changed its status from:", capacitor_statuses[i], "to", optimal_statuses[i])
